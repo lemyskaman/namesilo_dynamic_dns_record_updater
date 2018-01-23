15873ded5a8155c0fc8d2d00eee6f9ceb7d21412
lemyskaman <lemyskaman@gmail.com>
2018-01-23 10:37:35 -0400

    config no pushing test

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var _ = require('lodash');
 
 var config = {
      ipifyPublicHttpServiceQuery : "https://api.ipify.org?format=text",
-     namesilo_target_domain : "",
+     namesilo_target_domain : "---",
      namesilo_target_resource_host : "",
      namesilo_api_key : "",
      record_list_http_query_template : _.template("https://www.namesilo.com/api/dnsListRecords?version=1&type=xml&key=<%- apiKey %>&domain=<%- targetDomain %>"),
