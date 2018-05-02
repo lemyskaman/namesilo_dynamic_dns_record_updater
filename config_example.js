a62a9f859334b98378340418bcf8fbc52706a811
lemyskaman <lemyskaman@gmail.com>
2018-05-01 21:10:59 -0400

    adding multiple records update

--- config_example.js
+++ config_example.js
@@ -2,11 +2,12 @@ var _ = require('lodash');
 
 
 var config = {
+
      ipifyPublicHttpServiceQuery : "https://api.ipify.org?format=text",
      ipifyPollPeriod:"10000",//milisecs
-     namesilo_target_domain : " ",
-     namesilo_target_resource_host : " ",
-     namesilo_api_key : " ",
+     namesilo_target_domain : "kamansoft.com",
+     namesilo_target_resource_host : "lab",
+     namesilo_api_key : "b2fdae37c2932c3eb47f",
      record_list_http_query_template : _.template("https://www.namesilo.com/api/dnsListRecords?version=1&type=xml&key=<%- apiKey %>&domain=<%- targetDomain %>"),
      update_http_query_template : _.template("https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=<%- apiKey %>&domain=<%- targetDomain %>&rrid=<%- targetResourceId %>&rrhost=<%- targetResourceHost %>&rrvalue=<%- value %>&rrttl=7207 ")
     }
--- ip_updater.js
+++ ip_updater.js
@@ -39,17 +39,19 @@ function httpApiRequest(queryUrl) {
     return new Promise(function (resolve, reject) {
         //requesting
         //console.log(queryUrl.split("/"))
-        var urlDomain=queryUrl.split("/")[2]
+        var splitedQuery=queryUrl.split("/")
+        var urlDomain = splitedQuery[2];
         console.log("atempt http request to: " + urlDomain)
+        //console.log("full query: "+queryUrl )
         request(queryUrl, function (err, res, body) {
             if (err) {
                 reject(err);
             } else {
-                console.log("succes conecction to: "+urlDomain)
+                console.log("succes conecction to: " + urlDomain)
                 if (res.statusCode === 200) {
                     resolve(body)
                 } else {
-                    reject({ message: "no 200 status code on "+urlDomain+" request", response: response.toJSON })
+                    reject({ message: "no 200 status code on " + urlDomain + " request", response: response.toJSON })
                 }
             }
 
@@ -61,12 +63,85 @@ function httpApiRequest(queryUrl) {
 function namesiloApiPromisedReplyHandler(parsedResponseBody) {
     return new Promise(function (resolve, reject) {
         //console.log("handler "+parsedResponseBody.namesilo.reply[0].code[0])
-        if (parsedResponseBody.namesilo.reply[0].code[0] === '300' || parsedResponseBody.namesilo.reply[0].code[0] === '301'  ) {
+        if (parsedResponseBody.namesilo.reply[0].code[0] === '300' || parsedResponseBody.namesilo.reply[0].code[0] === '301') {
             resolve(parsedResponseBody)
         } else {
-            reject({message:"non valid dns api response code",resposeReply:parsedResponseBody.namesilo.reply[0]})
+            reject({ message: "non valid dns api response code", resposeReply: parsedResponseBody.namesilo.reply[0] })
         }
     })
+
+}
+
+/**
+ * checkValidRecord cheks for  a record on a recordlist
+ * @param recordList array with all elements to search on
+ * @param record String element to find on recordList
+ * @returns the found value or false  
+ */
+function isValidRecord(record, recordList) {
+    var result = _.find(recordList,
+        {
+            type: ['A'],
+            host: [record]
+        }
+    );
+    if (result !== undefined) {
+        return result
+    } else {
+        return false
+    }
+}
+
+/**
+ * ipUdater this function will do the main job, to update a namesilo record matching the value of fullNamesiloTargetHost
+ * param with one of the element on the recordlist 
+ * 
+ */
+function ipUpdater(recordList, ipToTargetHost, resourceHost) {
+
+    var target_record_data = isValidRecord(resourceHost+ "." + config.namesilo_target_domain, recordList);
+
+    if (target_record_data) {
+        var ipSetedOnTargetHost = target_record_data.value[0];
+        var targetResourceId = target_record_data.record_id[0];
+
+        console.log("server public ip is: " + ipToTargetHost + " and namesilo host record ip value is : " + ipSetedOnTargetHost)
+        if (ipToTargetHost !== ipSetedOnTargetHost) {
+            httpApiRequest(config.update_http_query_template(
+                {
+                    apiKey: config.namesilo_api_key,
+                    targetDomain: config.namesilo_target_domain,
+                    targetResourceId: targetResourceId,
+                    targetResourceHost: resourceHost,
+                    value: ipToTargetHost
+
+                }))
+                .then(function (result) {
+                    
+                    return parseString(result)
+                })
+                .then(function(result){
+                    return namesiloApiPromisedReplyHandler(result)
+                })
+                .then(function(result){
+                    console.log("record value for "+resourceHost+ "." + config.namesilo_target_domain+" succesfully updated")
+                    //console.log(result.namesilo.reply[0])
+                    
+                })
+                .catch(function (err) {
+                    console.log(err)
+                    //log.error(err) 
+                })
+
+        } else {
+            //console.log("no change on ip")
+            console.log("there is no need to update "+resourceHost+ "." + config.namesilo_target_domain+". Public ip and reosurce record value ip are equals")
+        }
+    } else {
+        console.log(resourceHost+ "." + config.namesilo_target_domain+" is not a valid record on namesilo")
+    }
+
+
 
 }
 
@@ -85,56 +160,30 @@ function main() {
         .then(function (result) {
             return namesiloApiPromisedReplyHandler(result)
         })
-        .then(function(result){
+        .then(function (result) {
             return Promise.all([result, httpApiRequest(config.ipifyPublicHttpServiceQuery)])
         })
         .then(function (results) {
-            
-            if (ValidateIPaddress(results[1])) {
-                var record_list = results[0].namesilo.reply[0].resource_record;
-                //console.log(record_list)
-                var target_record_data = _.find(record_list,
-                    {
-                        type: ['A'],
-                        host: [config.namesilo_target_resource_host + "." + config.namesilo_target_domain]
-                    }
-                )
-                console.log("public ip: " + results[1] + " namesilo host record ip value: " + target_record_data.value[0])
-                //log.info("public ip: " + results[1] + " namesilo host record ip value: " + target_record_data.value[0])
-                if (results[1] !== target_record_data.value[0]) {
-                    //log.info("public and host ip are diferent, atempting update dns record ")
-                    return httpApiRequest(config.update_http_query_template(
-                        {
-                            apiKey: config.namesilo_api_key,
-                            targetDomain: config.namesilo_target_domain,
-                            targetResourceId: target_record_data.record_id[0],
-                            targetResourceHost: config.namesilo_target_resource_host,
-                            value: results[1]
+            var ipToTargetHost = results[1];
+            if (ValidateIPaddress(ipToTargetHost)) {
+                var recordList = results[0].namesilo.reply[0].resource_record;
 
-                        }))
-                } else {
-                    console.log("no change on ip")
-                    return Promise.reject("there is no need to update. Public ip and reosurce record value ip are equals")
+                var promisedRecords = _.map(config.namesilo_target_resource_hosts, function (v, k, l) {
+                    
+                    ipUpdater(recordList,ipToTargetHost,v);
+                }, this)
 
-                }
             } else {
                 return Promise.reject("the retrived public ip addres isnt in a valid format")
             }
-        })
-        .then(function (result) {
-            return parseString(result)
-        })
-        .then(function(result){
-            return namesiloApiPromisedReplyHandler(result)
-        })
-        .then(function(result){
-            console.log("record updated")
-            //log.info("target record ip value UPDATED")
-        })
-        .catch(function(err){
+
+        })        
+        .catch(function (err) {
             console.log(err)
             //log.error(err) 
-        }) 
-    }
+        })
+}
+
+
 setInterval(main, config.ipifyPollPeriod);
 
